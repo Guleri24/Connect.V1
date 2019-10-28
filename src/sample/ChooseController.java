fc322a31c55e3433fa146655619e6c4608f7fe96
Guleri24 <abhishekguleri24@gmail.com>
2019-10-28 16:09:10 +0530

    add ViewStories class and fxml file

--- src/sample/ChooseController.java
+++ src/sample/ChooseController.java
@@ -1,4 +1,19 @@
 package sample;
 
-public class ChooseController {
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChooseController  implements Initializable {
+
+    public AnchorPane basePane;
+    public Pane myPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,10 @@
 package sample;
 import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,9 +16,17 @@ private Parent root;
     public void initialize(URL url, ResourceBundle resourceBundle) {
 setButton();
     }
-    public void setButton(){
+    public void setButton()  {
         Continue.setOnAction(e->{
-
+            System.out.println("hello new Controller");
+     ChooseController controller = new ChooseController();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Choose.fxml"));
+            loader.setController(controller);
+            try {
+                loader.load();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         });
     }
 }
