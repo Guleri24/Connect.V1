6a8b76ffc1df931137d6be45c77e6b441d3b7d9f
Guleri24 <abhishekguleri24@gmail.com>
2019-11-07 02:21:53 +0530

    Methods update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,51 +6,109 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Dialog;
+import javafx.scene.web.WebView;
 import javafx.stage.Window;
 
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-@FXML
+    @FXML
     public Button start;
     public Button About;
+    public Button Overcoming_the_monster;
+    public Button Comedy;
+    public Button Rags_to_Riches;
+    public Button The_Quest;
+    public Button Voyage_and_Return;
+    public Button Tragedy;
+    public Button Rebirth;
+    public Button Blue;
     @FXML
-private Main main;
-@Override
-    public void initialize(URL location, ResourceBundle resources){
+    public WebView view;
+    private Main main;
 
-}
-@FXML
- public void setStart(){
-    start.setOnAction(new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent event) {
-            try {
-                Main.showChoose();
-            } catch (Exception e) {
-                e.printStackTrace();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void setStart() {
+        start.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    Main.showChoose();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-        }
-    });
+        });
 
-}
-@FXML
-public void setAbout(ActionEvent event){
-    About.setOnAction(new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent event) {
-            Dialog dialog = new Dialog();
-            dialog.setContentText("Hello this is Connect");
-            Window window = dialog.getDialogPane().getScene().getWindow();
-            window.setOnCloseRequest(e->window.hide());
-            dialog.showAndWait();
-        }
-    });
-}
-@FXML
-    public void onClick(ActionEvent event) {
-    System.out.println("Hello FX");
+    }
+
+    @FXML
+    public void setAbout(ActionEvent event) {
+        About.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Dialog dialog = new Dialog();
+                dialog.setContentText("Hello this is Connect");
+                Window window = dialog.getDialogPane().getScene().getWindow();
+                window.setOnCloseRequest(e -> window.hide());
+                dialog.showAndWait();
+            }
+        });
+    }
+
+    @FXML
+    public void setOvercoming_the_monster() {
+        Overcoming_the_monster.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    view.getEngine().load("http://www.java2s.com/Code/Java/JavaFX/Menuitemeventhandler.htm");
+                    Main.View(view);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @FXML
+    public void setComedy() {
+
+    }
+
+    @FXML
+    public void setRags_to_Riches() {
+
+    }
+
+    @FXML
+    public void setThe_Quest() {
+
+    }
+
+    @FXML
+    public void setVoyage_and_Return() {
+
+    }
+
+    @FXML
+    public void setTragedy() {
+
+    }
+
+    @FXML
+    public void setRebirth() {
+
+    }
+
+    @FXML
+    public void setBlue() {
+
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -35,7 +37,16 @@ public static void showChoose() throws Exception{
         mainLayout=loader.load();
         Scene scene = new Scene(mainLayout,600,400);
         primaryStage.setScene(scene);
-        primaryStage.setResizable(true);
+        primaryStage.setResizable(false);
         primaryStage.show();
+}
+public static void View(WebView view) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view.fxml"));
+    BorderPane viewLayout = loader.<BorderPane>load();
+         Scene scene = new Scene(viewLayout,600,400);
+         primaryStage.setScene(scene);
+         primaryStage.setResizable(true);
+         primaryStage.show();
 }
 }
