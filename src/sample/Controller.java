c7b29c79864d0d849a73d37b6663e9217d91ea7c
Guleri24 <abhishekguleri24@gmail.com>
2019-11-10 23:20:35 +0530

    layout and button functioning update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,17 +3,25 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 import javafx.stage.Window;
 
 import java.net.URL;
+import java.security.KeyStore;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML
+    // Choose.fxml
     public Button start;
     public Button About;
     public Button Overcoming_the_monster;
@@ -24,91 +32,201 @@ public class Controller implements Initializable {
     public Button Tragedy;
     public Button Rebirth;
     public Button Blue;
+
+    //view.fxml
+    public MenuItem View_Comedy;
+    public MenuItem View_Rags_Rags;
+    public MenuItem View_Quest;
+    public MenuItem View_Voyage_Return;
+    public MenuItem View_Tragedy;
+    public MenuItem View_Blue;
+    public MenuItem View_Overcoming_monster;
+    public MenuItem View_Rebirth;
     @FXML
-    public WebView view;
-    private Main main;
+    WebView view;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
     }
 
     @FXML
     public void setStart() {
-        start.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                try {
-                    Main.showChoose();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        start.setOnAction(event -> {
+            try {
+                Main.showChoose();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
 
     }
+
 
     @FXML
     public void setAbout(ActionEvent event) {
-        About.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                Dialog dialog = new Dialog();
-                dialog.setContentText("Hello this is Connect");
-                Window window = dialog.getDialogPane().getScene().getWindow();
-                window.setOnCloseRequest(e -> window.hide());
-                dialog.showAndWait();
-            }
+        About.setOnAction(event1 -> {
+            Dialog dialog = new Dialog();
+            dialog.setContentText("Hello this is Connect");
+            Window window = dialog.getDialogPane().getScene().getWindow();
+            window.setOnCloseRequest(e -> window.hide());
+            dialog.showAndWait();
         });
     }
 
     @FXML
-    public void setOvercoming_the_monster() {
-        Overcoming_the_monster.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                try {
-                    view.getEngine().load("http://www.java2s.com/Code/Java/JavaFX/Menuitemeventhandler.htm");
-                    Main.View(view);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+    public void setChoose() {
+        Overcoming_the_monster.setOnAction(event -> {
+            try {
+                String url = "https://www.youtube.com";
+                Main.View();
+                System.out.println("hello Overcoming");
+          } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        Comedy.setOnAction(event -> {
+            try {
+                String url = "https://www.youtube.com";
+                Main.View();
+                System.out.println("hello Overcoming");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        Rags_to_Riches.setOnAction(event -> {
+            try {
+                String url = "https://www.youtube.com";
+                Main.View();
+                System.out.println("hello Overcoming");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        The_Quest.setOnAction(event -> {
+            try {
+                String url = "https://www.youtube.com";
+                Main.View();
+                System.out.println("hello Overcoming");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        Voyage_and_Return.setOnAction(event -> {
+            try {
+                String url = "https://www.youtube.com";
+                Main.View();
+                System.out.println("hello Overcoming");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        Tragedy.setOnAction(event -> {
+            try {
+                String url = "https://www.youtube.com";
+                Main.View();
+                System.out.println("hello Overcoming");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        Blue.setOnAction(event -> {
+            try {
+                String url = "https://www.youtube.com";
+                Main.View();
+                System.out.println("hello Overcoming");
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
     }
 
-    @FXML
-    public void setComedy() {
+    public void setMenu() {
+        View_Overcoming_monster.setOnAction(event -> {
+            try {
+                String url = "https://www.youtube.com";
+                Main.View();
+                System.out.println("hello Overcoming");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-    }
+        });
 
-    @FXML
-    public void setRags_to_Riches() {
+        View_Comedy.setOnAction(event -> {
+            try {
+                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
+                Main.View();
+                System.out.println("hello Comedy");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
-    }
+        View_Rebirth.setOnAction(event -> {
+            try {
+                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
+                Main.View();
+                System.out.println("hello Comedy");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
-    @FXML
-    public void setThe_Quest() {
+        View_Rags_Rags.setOnAction(event -> {
+            try {
+                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
+                Main.View();
+                System.out.println("hello Rags");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
-    }
+        View_Quest.setOnAction(event -> {
+            try {
+                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
+                Main.View();
+                System.out.println("hello Quest");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
-    @FXML
-    public void setVoyage_and_Return() {
+        View_Voyage_Return.setOnAction(event -> {
+            try {
+                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
+                Main.View();
+                System.out.println("hello Voyage");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
-    }
+        View_Tragedy.setOnAction(event -> {
+            try {
+                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
+                Main.View();
+                System.out.println("hello Tragedy");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
-    @FXML
-    public void setTragedy() {
-
-    }
-
-    @FXML
-    public void setRebirth() {
-
-    }
-
-    @FXML
-    public void setBlue() {
+        View_Blue.setOnAction(event -> {
+            try {
+                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
+                Main.View();
+                System.out.println("hello Blue");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
@@ -12,9 +13,12 @@ public class Main extends Application {
 private static Stage primaryStage;
 private static AnchorPane mainLayout;
 
+
     public static void main(String[] args){
         launch(args);
     }
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -31,7 +35,7 @@ private static void showMain() throws Exception{
     primaryStage.setResizable(false);
     primaryStage.show();
 }
-public static void showChoose() throws Exception{
+static void showChoose() throws Exception{
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Main.class.getResource("Choose.fxml"));
         mainLayout=loader.load();
@@ -40,13 +44,14 @@ public static void showChoose() throws Exception{
         primaryStage.setResizable(false);
         primaryStage.show();
 }
-public static void View(WebView view) throws Exception{
+static void View() throws Exception{
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Main.class.getResource("view.fxml"));
-    BorderPane viewLayout = loader.<BorderPane>load();
-         Scene scene = new Scene(viewLayout,600,400);
-         primaryStage.setScene(scene);
-         primaryStage.setResizable(true);
-         primaryStage.show();
+    BorderPane viewLayout = loader.load();
+        Scene scene = new Scene(viewLayout,868.0, 529.0);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
+        primaryStage.show();
 }
+
 }
