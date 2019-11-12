0236cfc2b58ba666544cc7850ebfbce6084a3b02
Guleri24 <abhishekguleri24@gmail.com>
2019-11-12 16:36:46 +0530

    base application completed

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,232 +1,69 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Dialog;
-import javafx.scene.control.MenuItem;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.stage.Window;
-
+import java.io.IOException;
 import java.net.URL;
-import java.security.KeyStore;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    @FXML
-    // Choose.fxml
     public Button start;
     public Button About;
     public Button Overcoming_the_monster;
-    public Button Comedy;
     public Button Rags_to_Riches;
+    public Button Comedy;
     public Button The_Quest;
     public Button Voyage_and_Return;
     public Button Tragedy;
     public Button Rebirth;
     public Button Blue;
+    public WebView view;
+    public Button Start;
 
-    //view.fxml
-    public MenuItem View_Comedy;
-    public MenuItem View_Rags_Rags;
-    public MenuItem View_Quest;
-    public MenuItem View_Voyage_Return;
-    public MenuItem View_Tragedy;
-    public MenuItem View_Blue;
-    public MenuItem View_Overcoming_monster;
-    public MenuItem View_Rebirth;
-    @FXML
-    WebView view;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void setStart(ActionEvent event) {
+      start.setOnAction(e->{
+          try {
+              Main.Choose();
+          } catch (IOException ex) {
+              ex.printStackTrace();
+          }
+      });
     }
 
-    @FXML
-    public void setStart() {
-        start.setOnAction(event -> {
+    public void setChoose(ActionEvent event) throws IOException {
+
+        Start.setOnAction(e->{
             try {
-                Main.showChoose();
-            } catch (Exception e) {
-                e.printStackTrace();
+                Main.View();
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
-        });
 
+        });
     }
 
-
-    @FXML
     public void setAbout(ActionEvent event) {
-        About.setOnAction(event1 -> {
+        About.setOnAction(e->{
             Dialog dialog = new Dialog();
-            dialog.setContentText("Hello this is Connect");
+            dialog.isResizable();
+            dialog.setContentText("Connect: connecting people by stories. \n " +
+                    "In this desktop application users can share and read stories of various type: \n" +
+                    " includes: Quest, Tragedy, Romance, Blue and many more. \n\n\n");
             Window window = dialog.getDialogPane().getScene().getWindow();
-            window.setOnCloseRequest(e -> window.hide());
+            window.setOnCloseRequest(w -> window.hide());
             dialog.showAndWait();
         });
     }
 
-    @FXML
-    public void setChoose() {
-        Overcoming_the_monster.setOnAction(event -> {
-            try {
-                String url = "https://www.youtube.com";
-                Main.View();
-                System.out.println("hello Overcoming");
-          } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
 
-        Comedy.setOnAction(event -> {
-            try {
-                String url = "https://www.youtube.com";
-                Main.View();
-                System.out.println("hello Overcoming");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        Rags_to_Riches.setOnAction(event -> {
-            try {
-                String url = "https://www.youtube.com";
-                Main.View();
-                System.out.println("hello Overcoming");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        The_Quest.setOnAction(event -> {
-            try {
-                String url = "https://www.youtube.com";
-                Main.View();
-                System.out.println("hello Overcoming");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        Voyage_and_Return.setOnAction(event -> {
-            try {
-                String url = "https://www.youtube.com";
-                Main.View();
-                System.out.println("hello Overcoming");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        Tragedy.setOnAction(event -> {
-            try {
-                String url = "https://www.youtube.com";
-                Main.View();
-                System.out.println("hello Overcoming");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        Blue.setOnAction(event -> {
-            try {
-                String url = "https://www.youtube.com";
-                Main.View();
-                System.out.println("hello Overcoming");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-    }
-
-    public void setMenu() {
-        View_Overcoming_monster.setOnAction(event -> {
-            try {
-                String url = "https://www.youtube.com";
-                Main.View();
-                System.out.println("hello Overcoming");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        });
-
-        View_Comedy.setOnAction(event -> {
-            try {
-                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
-                Main.View();
-                System.out.println("hello Comedy");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        View_Rebirth.setOnAction(event -> {
-            try {
-                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
-                Main.View();
-                System.out.println("hello Comedy");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        View_Rags_Rags.setOnAction(event -> {
-            try {
-                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
-                Main.View();
-                System.out.println("hello Rags");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        View_Quest.setOnAction(event -> {
-            try {
-                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
-                Main.View();
-                System.out.println("hello Quest");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        View_Voyage_Return.setOnAction(event -> {
-            try {
-                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
-                Main.View();
-                System.out.println("hello Voyage");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        View_Tragedy.setOnAction(event -> {
-            try {
-                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
-                Main.View();
-                System.out.println("hello Tragedy");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        View_Blue.setOnAction(event -> {
-            try {
-                String url = "<html><h1>Hello</h1><h2>Hello</h2></html>";
-                Main.View();
-                System.out.println("hello Blue");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,57 +1,141 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
-private static Stage primaryStage;
-private static AnchorPane mainLayout;
-
-
-    public static void main(String[] args){
-        launch(args);
-    }
-
-
+    private static Stage primaryStage;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Main.primaryStage = primaryStage;
-        showMain();
+
+        MainFxml();
     }
 
-private static void showMain() throws Exception{
-    FXMLLoader loader = new FXMLLoader();
-    loader.setLocation(Main.class.getResource("Main.fxml"));
-    mainLayout=loader.load();
-    Scene scene = new Scene(mainLayout,600,400);
-    primaryStage.setScene(scene);;
-    primaryStage.setResizable(false);
-    primaryStage.show();
-}
-static void showChoose() throws Exception{
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("Choose.fxml"));
-        mainLayout=loader.load();
-        Scene scene = new Scene(mainLayout,600,400);
+    private static void MainFxml() throws Exception {
+        AnchorPane MainLayout = FXMLLoader.load(Main.class.getResource("Main.fxml"));
+        Scene scene = new Scene(MainLayout);
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
         primaryStage.show();
-}
-static void View() throws Exception{
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("view.fxml"));
-    BorderPane viewLayout = loader.load();
-        Scene scene = new Scene(viewLayout,868.0, 529.0);
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(true);
-        primaryStage.show();
-}
+    }
 
+    static void Choose() throws IOException {
+        AnchorPane ChooseLayout = FXMLLoader.load(Main.class.getResource("Choose.fxml"));
+        Scene scene = new Scene(ChooseLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    static void View() throws IOException {
+        WebView view = new WebView();
+        WebEngine engine = view.getEngine();
+        engine.loadContent("<html> \n" +
+                "<style type=\"text/css\"> \n" +
+                "\n" +
+                "                body\n" +
+                "                { background-color:  #ffd5d5;\n" +
+                "                } \n" +
+                "                h1{\n" +
+                "                     text-align: center;\n" +
+                "                     font-size: 50px;\n" +
+                "                     color: #45454d;\n" +
+                "                }\n" +
+                "                h2{\n" +
+                "                    text-align: center;\n" +
+                "                    color: #45454d;\n" +
+                "                }\n" +
+                "                </style>\n" +
+                "                <h1> Welcome to Connect </h1>\n" +
+                "                <h2> Connecting people with stories.</h2>\n" +
+                "                <body>\n" +
+                "                    \n" +
+                "            \n" +
+                "                </body> </html>");
+        BackgroundFill background_fill = new BackgroundFill(Color.PINK,
+                CornerRadii.EMPTY, Insets.EMPTY);
+        // create Background
+        Background background = new Background(background_fill);
+
+        BorderPane borderPane = new BorderPane();
+
+        // Top
+        HBox hBox = new HBox();
+        hBox.setPrefSize(868.0, 42.0);
+        MenuButton Stories = new MenuButton("Stories");
+        MenuItem View_Overcoming_monster = new MenuItem("Overcoming the monster");
+        View_Overcoming_monster.setOnAction(e->{
+            engine.load("https://drive.google.com/drive/folders/1zAMIuF0MnICBnCspylcadCmIFcQ7vEWR?usp=sharing");
+        });
+        MenuItem View_Comedy = new MenuItem("Comedy");
+        View_Comedy.setOnAction(e->{
+            engine.load("https://drive.google.com/drive/folders/1xdJ50rXCaDT4ldmbfMV2UnVAoZGd2jDZ?usp=sharing");
+        });
+        MenuItem View_Rags_Rags = new MenuItem("Rags to Riches");
+        View_Rags_Rags.setOnAction(e->{
+            engine.load("https://drive.google.com/drive/folders/11qR1z6Y7IBDlBaCE9fR8BTKDpwZlDt9l?usp=sharing");
+        });
+        MenuItem View_Quest = new MenuItem("The Quest");
+        View_Quest.setOnAction(e->{
+            engine.load("https://drive.google.com/drive/folders/17ldeRUfEti2aDKeGzOvy4TrNionP7mWn?usp=sharing");
+        });
+        MenuItem View_Voyage_Return = new MenuItem("Voyage and Return");
+        View_Voyage_Return.setOnAction(e->{
+            engine.load("https://drive.google.com/drive/folders/1XcGCsiZMT0wMVkhujQRx2TVU6lZd18vk?usp=sharing");
+        });
+        MenuItem View_Rebirth = new MenuItem("Rebirth");
+        View_Rebirth.setOnAction(e->{
+            engine.load("https://drive.google.com/drive/folders/14Gt-C4mJeP3h2CGNJKIYfAkF-ttoNxo1?usp=sharing");
+        });
+        MenuItem View_Tragedy = new MenuItem("Tragedy");
+        View_Tragedy.setOnAction(e->{
+            engine.load("https://drive.google.com/drive/folders/1nut8tJt5qqCLOcoZb3s6FTGlufFshG5r?usp=sharing");
+        });
+        MenuItem View_Blue = new MenuItem("Blue");
+        View_Blue.setOnAction(e->{
+            engine.load("https://drive.google.com/drive/folders/18baEyCpKqoNmvzMzDgvnbXxUBXIxlpkf?usp=sharing");
+        });
+        Stories.getItems().addAll(View_Overcoming_monster, View_Comedy, View_Rags_Rags, View_Quest, View_Voyage_Return, View_Rebirth, View_Tragedy, View_Blue);
+        Stories.setPrefSize(116.0, 40.0);
+        HBox.setMargin(Stories, new Insets(20, 20, 20, 20));
+        ObservableList<javafx.scene.Node> list = hBox.getChildren();
+        list.addAll(Stories);
+        hBox.setBackground(background);
+
+
+
+        borderPane.setTop(hBox);
+        // Centre
+        borderPane.setCenter(view);
+
+        // Bottom
+        AnchorPane pane = new AnchorPane();
+        pane.setPrefSize(868.0, 17.0);
+        pane.setBackground(background);
+        borderPane.setBottom(pane);
+
+
+        Scene scene = new Scene(borderPane, 868.0, 529.0);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
